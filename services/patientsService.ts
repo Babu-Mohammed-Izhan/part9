6635c3fdc7eb2d8194bfd5cd26626babefe57f86
Izhan <babuizhan1234@gmail.com>
2021-10-25 11:22:30 +0530

    added hospitalentry types

--- services/patientsService.ts
+++ services/patientsService.ts
@@ -1,7 +1,8 @@
-import patientData from "../data/patients.json";
+import patients from "../data/patients";
+// import patientData from "../data/patients.json";
 import { PublicPatientEntry, PatientEntry, Entry } from "../types";
 
-const patinets: Array<PatientEntry> = patientData;
+const patinets: Array<PatientEntry> = patients;
 
 const getPatients = (): Array<PublicPatientEntry> => {
   return patinets.map(({ id, name, dateOfBirth, gender, occupation }) => ({
--- types.ts
+++ types.ts
@@ -4,13 +4,6 @@ export interface DiagnoseEntry {
   latin?: string;
 }
 
-export interface Entry {
-  description: string;
-  creationDate: string;
-  specialist: string;
-  code: string;
-}
-
 export interface PatientEntry {
   id: string;
   name: string;
@@ -27,3 +20,49 @@ export enum Gender {
   male = "male",
   female = "female",
 }
+
+interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: Array<DiagnoseEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;

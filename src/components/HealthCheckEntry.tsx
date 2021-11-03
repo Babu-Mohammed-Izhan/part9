b6beca730717fb72aba63a8ad64571afc3804f65
Izhan <babuizhan1234@gmail.com>
2021-11-03 17:00:59 +0530

    finished healthcheck entry

--- src/components/HealthCheckEntry.tsx
+++ src/components/HealthCheckEntry.tsx
@@ -5,14 +5,38 @@ import { Card, Icon } from "semantic-ui-react";
 const HealthCheckEntryC: React.FC<{ entry: HealthCheckEntry }> = ({
   entry,
 }) => {
-  // const healthColor = ["green", "yellow", "orange", "red"];
+  // const healthColor = ['green', 'yellow', 'orange', 'red'];
 
-  const header = `${entry.date}`;
+  const header = (
+    <h3>
+      {entry.date}
+      <Icon name="doctor" />
+    </h3>
+  );
   const meta = `${entry.description}`;
 
-  const extra = <Icon name="heart" color="red" />;
+  const extra = (e: number) => {
+    switch (e) {
+      case 0:
+        return <Icon name="heart" color="green" />;
+        break;
+      case 1:
+        return <Icon name="heart" color="yellow" />;
+        break;
+      case 2:
+        return <Icon name="heart" color="orange" />;
+        break;
+      case 3:
+        return <Icon name="heart" color="red" />;
+        break;
+      default:
+        break;
+    }
+  };
 
-  return <Card header={header} meta={meta} extra={extra} />;
+  return (
+    <Card header={header} meta={meta} extra={extra(entry.healthCheckRating)} />
+  );
 };
 
 export default HealthCheckEntryC;
